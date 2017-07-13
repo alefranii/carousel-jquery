3b7b762a11daea1a7672711852c72c54e977779b
alefranii <afmormar@gmail.com>
2017-07-13 04:38:23 -0400

    fotos enmarcadas, me falta funcionalidad de fecha para pausar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 
 $(function(){
-    $('#slider div:gt(0)').hide();
+    $('#slider div:gt(0)').hide(); //Con gt(x) seleccionamos todos los div a partir del número x y lo escondemos 
     setInterval(function(){
       $('#slider div:first-child').fadeOut(0)
-         .next('div').fadeIn(1000)
+         .next('div').fadeIn(500)
          .end().appendTo('#slider');}, 5000);
 });

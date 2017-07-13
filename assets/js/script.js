e56bf87e2fa4198008992ae110edc9fda6ced15d
alefranii <afmormar@gmail.com>
2017-07-13 05:22:48 -0400

    intento de vinculo control ruleta 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,15 @@
 $(function(){
     $('#slider div:gt(0)').hide(); //Con gt(x) seleccionamos todos los div a partir del número x y lo escondemos 
     setInterval(function(){
-      $('#slider div:first-child').fadeOut(0)
-         .next('div').fadeIn(500)
-         .end().appendTo('#slider');}, 5000);
+    	$('#slider div:first-child').fadeOut(0)
+    	.next('div').fadeIn(500)
+    	.end().appendTo('#slider');}, 5000);
+
+    $(".slick-carousel").click(function(){
+    	$("#slider").carousel(1);
+    });
+
+    $(".carousel-control").click(function(){
+    	$("#slider").carousel("prev");
+    });
 });
